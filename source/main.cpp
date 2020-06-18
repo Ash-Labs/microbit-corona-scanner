670d82b38453a4a27d31a9d92c1cc36c19d465f7
hunz <zn000h@gmail.com>
2020-06-18 14:46:44 +0200

    RSSI tests

--- source/main.cpp
+++ source/main.cpp
@@ -41,11 +41,39 @@ void timer(void) {
 	}
 }
 
+/*
+#define MIN(a,b) (((a)<=(b))?(a):(b))
+
+uint8_t last_rssi = 255;
+uint8_t min_rssi = 255;
+
+void rssi_test(const uint8_t rssi) {
+	char buf[8];
+	if(uBit.systemTime() - last_rx < 5000)
+		return;
+	last_rx = uBit.systemTime();
+	sprintf(buf,"%03d",rssi);
+	uBit.display.scrollAsync(rssi);
+}
+*/
+
+/* RSSI:
+ * 
+ * min RSSI: 158
+ * max RSSI: ~200
+ * 
+ * ~5cm distance: 201..204
+ * ~1m  distance: 188..191
+ * ~3m  distance: 158..172
+ * ~5m  distance + wall: 158
+ * ~8m distance + 2x wall: 158
+ */
+
 void advertisementCallback(const Gap::AdvertisementCallbackParams_t *params) {
     const uint8_t len = params->advertisingDataLen;
 	const uint8_t *p = params->advertisingData;
 	const uint8_t rssi = params->rssi; /* use for LED brightness */
-	
+		
 	/* match Exposure Notification Service Class UUID 0xFD6F 
 	 * 
 	 * spec: https://www.blog.google/documents/70/Exposure_Notification_-_Bluetooth_Specification_v1.2.2.pdf page 4 
@@ -55,6 +83,12 @@ void advertisementCallback(const Gap::AdvertisementCallbackParams_t *params) {
 	 * 17 16 6ffd 660a6af67f7e946b3c3ce253dae9b411 78b0e9c2 (rpi, aem)
 	 * */
 	if((len == 28) && (p[0] == 3) && (p[1] == 3) && (p[2] == 0x6f) && (p[3] == 0xfd)) {
+		//last_rssi = rssi;
+		//min_rssi = MIN(min_rssi, rssi);
+
+//		rssi_test(rssi);
+//		return;
+
 		last_rx = uBit.systemTime();
 		uBit.display.printChar('.');
 		rxcount++;
@@ -72,6 +106,8 @@ int main() {
         //uBit.ble->waitForEvent();
 		uBit.sleep(50);
 		timer();
+		//uBit.sleep(200);
+		//uBit.serial.printf("rssi %d min %d\r\n",last_rssi,min_rssi);
     }
     return 0;
 }
