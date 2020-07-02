3f87b71f73a609ffd74b67ad44d889f265713a67
hunz <zn000h@gmail.com>
2020-07-02 19:50:51 +0200

    WIP

--- source/is31fl3738.cpp
+++ source/is31fl3738.cpp
@@ -3,6 +3,19 @@
 
 #include "is31fl3738.h"
 
+/* CMD and DATA are mostly for readability */
+#define CMD(c)         (c)
+#define DATA(n)        (n)
+#define INIT_END       0
+
+static const uint8_t init_data[] __attribute__ ((aligned (4))) = {
+	CMD(IS31FL3738_CMD_LEDCTL), DATA(4), 0x00, 0x03, 0x00, 0x03,      /* turn LEDs on */
+	CMD(IS31FL3738_CMD_FUNC),   DATA(3), 0x00, 0x01, 0xff,            /* normal operation, set GCRR to 255 */
+	CMD(IS31FL3738_CMD_PWM),
+	DATA(3), 0x00, 0xff, 0xff, /* test */
+	INIT_END
+};
+
 /* example:
 	# cmd write enable
 	write 0xfe 0xC5
@@ -90,7 +103,18 @@ extern MicroBit uBit;
 
 #define SLAVE_ADDR	(0x50<<1)
 
+static int is31fl3738_cmd(uint8_t cmd) {
+	uint8_t buf[2] = {IS31FL3738_REG_WRLOCK, IS31FL3738_WRLOCK_MAGIC};
+	int res = uBit.i2c.write(SLAVE_ADDR, (char*)buf, 2);
+	if(res != MICROBIT_OK)
+		return res;
+	buf[0] = IS31FL3738_REG_CMD;
+	buf[1] = cmd;
+	return uBit.i2c.write(SLAVE_ADDR, (char*)buf, 2);
+}
+
 int is31fl3738_init(void) {
+	const uint8_t *init;
 	char tmp;
 	int res = uBit.i2c.read(SLAVE_ADDR, &tmp, 1);
 	if(res != MICROBIT_OK)
@@ -98,7 +122,17 @@ int is31fl3738_init(void) {
 
 	uBit.i2c.frequency(400000);
 	
-	/* TODO: init */
+	for(init = init_data; *init; init++) {
+		uint8_t v = *init;
+		if(v >= IS31FL3738_REG_CMD)
+			res = is31fl3738_cmd(v);
+		else {
+			res = uBit.i2c.write(SLAVE_ADDR, (const char *)init+1, v);
+			init+=v;
+		}
+		if(res != MICROBIT_OK)
+			return res;
+	}
 
 	return MICROBIT_OK;
 }
